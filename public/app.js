df4fa9a20b130618dbc94cfb4b39f48d2defaf9d
Nathan Hall <nathan.mh@gmail.com>
2016-03-03 11:47:22 -0500

    create and get lecturers

--- public/app.js
+++ public/app.js
@@ -3,21 +3,59 @@ $(document).ready(function() {
 })
 
 var hanLec = {
+  url: {
+    getLecturer: "/lecturers/",
+    createLecturer: "/lecturers/"
+  },
   init: function() {
     hanLec.events()
     hanLec.styling()
   },
   events: function() {
-
+    $('input[name["create-lecturer"]]').on('click', function(event) {
+      event.preventDefault();
+      var lecturer = hanLec.getLecturerInfo();
+      hanLec.createLecturer(lecturer);
+    })
   },
   styling: function() {
 
   },
   getLecturers: function() {
+    $.ajax({
+      method: 'GET',
+      url: hanLec.url.getLecturer,
+      success: function(lecturerData) {
+        console.log("RECEIVED LECTURERS", lecturerData);
 
+      },
+      error: function(err) {
+        console.log('oh shit', err);
+      }
+    })
   },
   createLecturer: function(lecturer) {
-
+    $.ajax({
+      method: 'POST',
+      url: hanLec.url.createLecturer,
+      data: lecturer,
+      success: function(createdLecturer) {
+        console.log("CREATED LECTURER", createdLecturer);
+      },
+      error: function(err) {
+        console.log("not workee", err);
+      }
+    })
+  },
+  getLecturerInfo: function() {
+    var name = $('input[name="name"]').val();
+    var topic = $('input[name="topic"]').val();
+    var image = $('input[name="image"]').val();
+    return {
+      name: name,
+      topic: topic,
+      image: image
+    };
   },
   updateLecturer: function(id) {
 
